56d4160744748cead06e77dda14a7227f60f03f7
xiaolinzi <844202100@qq.com>
2024-03-27 09:37:12 +0800

    use thread.Thread

--- main.py
+++ main.py
@@ -8,13 +8,14 @@
 @Desc    :   a switch backup model
 """
 
-
 import os
 import re
+import threading
+import customtkinter
 from datetime import datetime
 from netmiko import SSHDetect, ConnectHandler
-import customtkinter
 from concurrent.futures import ThreadPoolExecutor
+
 
 def netmiko_ssh_detect_stype(ip: str, usrname: str, password: str) -> str:
     """netmiko_ssh_detect_stype
@@ -36,78 +37,6 @@ def netmiko_ssh_detect_stype(ip: str, usrname: str, password: str) -> str:
     guesser = SSHDetect(**dev)
     best_match = guesser.autodetect()
     return best_match
-
-
-def backup_sw(ip: str, usrname: str, password: str) -> str:
-    """backup_sw
-    AArgs:
-        ip (str): login/manage ip
-        usrname (str): login/manage username
-        password (str): login/manage password
-
-    Returns:
-        str: info of result
-    """
-    device_type = netmiko_ssh_detect_stype(ip, usrname, password)
-    if device_type is None:
-        device_type = "hp_comware"
-    try:
-        with ConnectHandler(
-            device_type=device_type,
-            ip=ip,
-            username=usrname,
-            password=password,
-            conn_timeout=5,
-            fast_cli=False,
-            auth_timeout=10,
-        ) as net_connect:
-            if "cisco" not in device_type:
-                configuration = net_connect.send_command(
-                    "display current-configuration",
-                    read_timeout=30,
-                    expect_string="return",
-                )
-                if device_type == "hp_comware":
-                    match = re.search(r"#.*?return", configuration, re.DOTALL)
-                    if match:
-                        matched_text = match.group(0)
-                        configuration = matched_text
-            else:
-                configuration = net_connect.send_command(
-                    "show running-config", read_timeout=30
-                )
-            # create Datetime
-            date = datetime.today().strftime("%Y-%m-%d")
-            if not os.path.exists(date):
-                os.makedirs(date)
-
-            with open(os.path.join(date, f"{ip}.config"), "w", encoding="utf-8") as f:
-                f.write(configuration)
-            return f"{ip}_success"
-
-    except Exception as e:
-        return f"{ip}_{e.__class__.__name__}"
-
-
-def backup_switches(lines: str) -> str:
-    """readlines to backup_switches
-
-    Args:
-        lines (str): lines
-
-    Returns:
-        str: result
-    """
-    # use threadpool in mainloop instead of waiting
-    res = []
-    with ThreadPoolExecutor(8) as pool:
-        for line in lines.split("\n"):
-            line = line.strip()
-            if len(line) > 1:
-                ip, username, password = re.split(r"\s+", line)
-                future = pool.submit(backup_sw, (ip, username, password))
-                res.append(future.result())
-    return "\n".join(res)
 
 
 def get_local_storage() -> str:
@@ -135,6 +64,7 @@ class ToplevelWindow(customtkinter.CTkToplevel):
 
 
 class App(customtkinter.CTk):
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.toplevel_window = None
@@ -157,19 +87,108 @@ class App(customtkinter.CTk):
         self.button.grid(row=2, column=0, padx=20, pady=8, sticky="ew", columnspan=2)
 
         self.toplevel_window = None
+        self.top_text = "Start backup"
 
     def button_backup(self):
         """button_backup"""
         text = self.textbox.get("0.0", "end")
+        # save text to file for next loading
         set_local_storage(text)
+        # set top text to start
+        self.top_text = "Start  Backup\n"
+
         # backup switch
-        res = backup_switches(text)
+        self.backup_switches(text)
+
         if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
-            self.toplevel_window = ToplevelWindow(text=res)
-            # top one the info window
+            self.toplevel_window = ToplevelWindow(text=self.top_text)
             self.toplevel_window.attributes("-topmost", 1)
-        else:
+
+    def update_message(self, res):
+        # add output text in top_text and update top windows
+        self.top_text += res
+        # update top window
+
+        if self.toplevel_window and self.toplevel_window.winfo_exists():
+            self.after(
+                0, lambda: self.toplevel_window.label.configure(text=self.top_text)
+            )
             self.toplevel_window.focus()
+
+    def backup_sw(self, item) -> str:
+        """backup_sw
+        AArgs:
+            ip (str): login/manage ip
+            usrname (str): login/manage username
+            password (str): login/manage password
+
+        Returns:
+            str: info of result
+        """
+
+        ip, usrname, password = item
+        try:
+            device_type = netmiko_ssh_detect_stype(ip, usrname, password)
+        except:
+            device_type = "hp_comware"
+        try:
+            with ConnectHandler(
+                device_type=device_type,
+                ip=ip,
+                username=usrname,
+                password=password,
+                conn_timeout=5,
+                fast_cli=False,
+                auth_timeout=10,
+            ) as net_connect:
+                if "cisco" not in device_type:
+                    configuration = net_connect.send_command(
+                        "display current-configuration",
+                        read_timeout=30,
+                        expect_string="return",
+                    )
+                    if device_type == "hp_comware":
+                        match = re.search(r"#.*?return", configuration, re.DOTALL)
+                        if match:
+                            matched_text = match.group(0)
+                            configuration = matched_text
+                else:
+                    configuration = net_connect.send_command(
+                        "show running-config", read_timeout=30
+                    )
+                # create Datetime
+                date = datetime.today().strftime("%Y-%m-%d")
+                if not os.path.exists(date):
+                    os.makedirs(date)
+
+                with open(
+                    os.path.join(date, f"{ip}.config"), "w", encoding="utf-8"
+                ) as f:
+                    f.write(configuration)
+
+                return f"{ip}_success"
+
+        except Exception as e:
+            self.update_message(f"{ip}_{e.__class__.__name__}")
+            return f"{ip}_{e.__class__.__name__}"
+
+    def backup_switches(self, lines: str) -> str:
+        """readlines to backup_switches
+        Args:
+            lines (str): lines
+        Returns:
+            str: result
+        """
+        # use threadpool in mainloop instead of waiting
+
+        for line in lines.split("\n"):
+            line = line.strip()
+            if len(line) > 1:
+                ip, username, password = re.split(r"\s+", line)
+                thread = threading.Thread(
+                    target=self.backup_sw, args=((ip, username, password),)
+                )
+                thread.start()
 
 
 if __name__ == "__main__":
